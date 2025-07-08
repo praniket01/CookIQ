04cdfa9a719d020dac59aba0c54c58b34d8d49f1
praniket01 <praniketbodke01@gmail.com>
2025-07-08 23:43:27 +0530

    package fix

--- Cookiq/backend/setup.sh
+++ Cookiq/backend/setup.sh
@@ -3,9 +3,8 @@
 # Exit on any error
 set -e
 
-cd "$(dirname "$0")"  # ensures we're in the backend directory
+cd "$(dirname "$0")"  
 npm install
-npm install @prisma/client
 
 echo " Running Prisma generate..."
 npx prisma generate --schema=../prisma/schema.prisma
